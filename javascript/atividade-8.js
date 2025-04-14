b12dd28b1d72e6a4ee58975063ae62fe52824fda
Samuel Lima <105258182+Samuelrlm@users.noreply.github.com>
2025-04-14 13:42:10 -0300

    javascript

---
+++ javascript/atividade-8.js
@@ -0,0 +1,8 @@
+//Objeto - Object
+const user = {
+    nome: "Samuel",
+    idade: 24,
+    cidade: "Fortaleza"
+}
+
+console.log(user.nome)
---
+++ javascript/atividade-9.js
@@ -0,0 +1,13 @@
+const user = {
+    email: "samuel@gmail.com",
+    senha: "12345"
+}
+
+if(user.email == "samuel@gmail.com" 
+    && user.senha == "1234568"){
+   console.log("Login realizado com sucesso")
+} else {
+    console.log("Email ou senha inválidos")
+}
+
+
